3fdd8321a5b97792980bd2bfaf809e5822c6601f
Izzah <l201314@lhr.nu.edu.pk>
2023-01-21 15:34:04 +0500

    Updates

---
+++ precache-manifest.8caa771934cfa8f4f2c835983cf26cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ce32e20fe69d0a19100cf14f4ab35e",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "4f67f098a07a648938d8",
+    "url": "/covid-tracker/static/css/main.323db777.chunk.css"
+  },
+  {
+    "revision": "9c6d354196774389c1c7",
+    "url": "/covid-tracker/static/js/2.27e91301.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.27e91301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f67f098a07a648938d8",
+    "url": "/covid-tracker/static/js/main.c2db54db.chunk.js"
+  },
+  {
+    "revision": "d18efbfbd5f9bcddd6ff",
+    "url": "/covid-tracker/static/js/runtime-main.fe48da39.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covidImg2.d7265326.png"
+  }
+]);
